224476814a402d2b0772da231e92518c02ddcbec
Casper Svenning Jensen <casper@svenningjensen.dk>
2014-07-01 21:20:11 +0200

    Initial commit

---
+++ parser.go
@@ -0,0 +1,103 @@
+
+package main
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+    "errors"
+    "strings"
+    "strconv"
+)
+
+func ParseBenchmarkFile(path string) (formula Formula, err error) {
+
+    // Open file
+
+    file, err := os.Open(path)
+    if err != nil {
+        return 
+    }
+    defer file.Close()
+
+    // Scan file
+
+    var insertedClauses = 0
+
+    var scanner = bufio.NewScanner(file)
+    for scanner.Scan() {
+
+        var line = scanner.Text()
+        var ts = strings.Split(line, " ")
+        var tokens = make([]string, len(ts))
+
+        // some benchmark sets inject random spaces.
+        // filter out all "empty" tokens
+
+        var top = 0
+        for _, t := range ts {
+            if t != "" {
+                tokens[top] = t
+                top++
+            }
+        }
+
+        tokens = tokens[:top]
+
+        if len(tokens) == 0 || tokens[0] == "c" || tokens[0] == "%" || tokens[0] == "0" {
+            continue // ignore blank lines or comments
+
+        } else if len(tokens) == 4 && tokens[0] == "p" && tokens[1] == "cnf" {
+
+            var _, err1 = strconv.Atoi(tokens[2])
+            var numClauses, err2 = strconv.Atoi(tokens[3])
+
+            if (err1 == nil && err2 == nil) {
+                formula = make(Formula, numClauses)
+                continue
+            }
+
+        } else if formula != nil {
+
+            var clause = make(Clause, len(tokens)-1)
+            
+            for i, token := range tokens {
+
+                var atom int
+
+                atom, err = strconv.Atoi(token)
+                if err != nil {
+                    return
+                }
+                
+                var isNegated = false
+                if (atom < 0) {
+                    isNegated = true
+                    atom = atom * -1
+                }
+
+                if (atom != 0) {
+                    var literal Literal
+                    literal.atom = Atom(atom)
+                    literal.negated = isNegated
+
+                    clause[i] = literal
+                }
+
+            }
+
+            formula[insertedClauses] = clause
+            insertedClauses++
+
+            continue
+        }
+
+        err = errors.New(fmt.Sprint("Parsing error at line: ", line, "\n"))
+        return
+        
+    }
+
+    err = scanner.Err()
+    return
+
+}
---
+++ sat.go
@@ -0,0 +1,204 @@
+
+package main
+
+import (
+    "fmt"
+)
+
+type Formula []Clause
+
+func (formula Formula) NumAtoms() uint64 {
+    var atomIdMax uint64 = 0
+
+    for _, clause := range formula {
+        for _, literal := range clause {
+
+            if uint64(literal.atom) > atomIdMax {
+                atomIdMax = uint64(literal.atom)
+            }
+        }
+    }
+
+    return atomIdMax + 1
+}
+
+type Clause []Literal
+
+type Literal struct {
+    negated bool
+    atom Atom
+}
+
+type Atom uint64
+
+type Solution []bool
+
+type assignment struct {
+    value bool
+    assigned bool
+}
+
+type stackAssignment struct {
+    atom Atom
+    value bool
+}
+
+type assignments struct {
+    raa []assignment
+    stackPointer uint64
+    stackMax uint64
+    stack []stackAssignment
+}
+
+func newAssignments(numAtoms uint64) *assignments {
+    
+    var assignments = new(assignments)
+
+    assignments.raa = make([]assignment, numAtoms)
+    assignments.stackPointer = 0
+    assignments.stackMax = numAtoms
+    assignments.stack = make([]stackAssignment, numAtoms)
+    
+    return assignments
+}
+
+func (assignments *assignments) Solution() Solution {
+    var solution = make(Solution, len(assignments.raa))
+
+    for i, assignment := range assignments.raa {
+        solution[i] = assignment.value
+    }
+
+    return solution
+}
+
+func (assignments *assignments) HasUnassigned() bool {
+    return assignments.stackPointer < assignments.stackMax
+}
+
+func (assignments *assignments) GetFirstUnassigned() (Atom, bool) {
+
+    for i := range assignments.raa {
+        if !assignments.raa[i].assigned {
+            return Atom(i), true
+        }
+    }
+
+    return Atom(0), false
+}
+
+func (assignments *assignments) PushAssignment(atom Atom, value bool) bool {
+
+    // TODO this assumes that the atom is not already assigned!
+
+    if uint64(atom) < assignments.stackMax {
+        assignments.raa[uint64(atom)] = assignment{value, true}
+        assignments.stack[assignments.stackPointer] = stackAssignment{atom, value}
+        assignments.stackPointer++
+        return true
+    }
+    
+    return false
+}
+
+func (assignments *assignments) PopAssignment() (assignment, bool) {
+    
+    if assignments.stackPointer == 0 {
+        return assignment{}, false
+    }
+
+    assignments.stackPointer--
+
+    var atom = assignments.stack[assignments.stackPointer].atom
+
+    assignments.raa[uint64(atom)].assigned = false
+    return assignments.raa[uint64(atom)], true
+}
+
+
+func (assignments *assignments) PartiallySatisfies(formula Formula) bool {
+
+    for _, clause := range formula {
+
+        var isSatisfied = false
+        var hasUnassigned = false
+
+        for _, literal := range clause {
+            
+            var assignment = assignments.raa[uint64(literal.atom)]
+
+            hasUnassigned = hasUnassigned || !assignment.assigned
+
+            isSatisfied = 
+                isSatisfied ||
+                (literal.negated && !assignment.value) ||
+                (!literal.negated && assignment.value)
+        }
+
+        if !isSatisfied && !hasUnassigned {
+            return false
+        }
+    }
+
+    return true
+
+}
+
+func Solve(formula Formula) (Solution, bool) {
+
+    var numAtoms = formula.NumAtoms()
+    var assignments = newAssignments(numAtoms)
+
+    var forceAlternative = false
+
+    for {
+
+        // success condition
+        
+        if !assignments.HasUnassigned() {
+            return assignments.Solution(), true
+        }
+
+        // pick -- try new assignment
+
+        var atom, ok = assignments.GetFirstUnassigned()
+        if !ok {
+            panic(fmt.Sprintf("Could not find an unassigned value, even though one should exist"))
+        }
+
+        assignments.PushAssignment(atom, forceAlternative)
+        forceAlternative = false
+
+        // deduce -- unit propagation
+
+        var conflict = false
+
+        // TODO
+
+        // resolve - if we have a conflict
+
+        conflict = conflict || !assignments.PartiallySatisfies(formula)
+
+        if conflict {
+
+            // backtrack 
+
+            for {
+
+                var last, ok = assignments.PopAssignment()
+
+                if !ok {
+                    // We can't backtrack further, so stop the search
+                    return nil, false
+                }
+
+                if last.value == false {
+                    // We have backtracked to a false branch, stop and explore the true branch next iteration
+                    forceAlternative = true
+                    break
+                }
+            }
+        }
+    }
+
+}
